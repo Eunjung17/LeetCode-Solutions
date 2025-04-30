826a507f3ebe98915080756d2285cd470b004e32
Eunjung17 <sogoal17@naver.com>
2025-04-30 11:38:53 -0500

    Time: 2 ms (70.71%), Space: 55.8 MB (27.8%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,8 +1,8 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
+/*
+1. First Code
+2. Time Complexity O(n^2)
+3. Space Complexity O(1)
+
 function twoSum(nums, target){
     for(let i = 0 ; i < nums.length ; i++){
         for(let j = i+1 ; j < nums.length ; j++){
@@ -14,3 +14,24 @@ function twoSum(nums, target){
 
     return false;
 }
+
+*/
+
+/*
+1. Second Code
+2. Time Complexity O(n)
+3. Space Complexity O(1)
+*/ 
+function twoSum(nums, target){
+    let indexSet = new Map();
+    for(let i = 0 ; i < nums.length ; i++){
+        const difference = target - nums[i];
+        if(indexSet.has(nums[i])) return [indexSet.get(nums[i]), i];
+        else indexSet.set(difference, i);    
+    }
+    return indexSet;
+}
+
+let nums = [2,7,11,15];
+const target = 9;
+console.log(twoSum(nums, target));
