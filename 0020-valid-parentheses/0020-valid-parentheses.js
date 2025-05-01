64fdd4ca488d5b25cc1b09bd931679c3e78ebad5
Eunjung17 <sogoal17@naver.com>
2025-05-01 08:46:06 -0500

    Time: 2 ms (75.95%), Space: 55.8 MB (51.75%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -1,28 +1,52 @@
 /**
+ * First Code
+ * @param {string} s
+ * @return {boolean}
+ */
+// function isValid(s) {
+
+//     let openBracket = [];
+//     for(let i = 0; i < s.length; i++){
+//         if(s[i] === '(' || s[i] === '[' || s[i] === '{') openBracket.push(s[i]);
+//         else{
+//             if(s[i] === ')'){
+//                 if(openBracket[openBracket.length-1] === '(') openBracket.pop();
+//                 else return false;
+//             }else if(s[i] === ']'){
+//                 if(openBracket[openBracket.length-1] === '[') openBracket.pop();
+//                 else return false;
+//             }else if(s[i] === '}'){
+//                 if(openBracket[openBracket.length-1] === '{') openBracket.pop();
+//                 else return false;
+//             }
+//         }
+//     }
+
+//     if(openBracket.length === 0) return true;
+//     else return false;
+// };
+
+/**
+ * Second Code
  * @param {string} s
  * @return {boolean}
  */
 function isValid(s) {
+    const bracketPairs = {
+        ')' : '(',
+        ']' : '[',
+        '}' : '{'
+    };
 
-    let openBracket = [];
-    for(let i = 0; i < s.length; i++){
-        if(s[i] === '(' || s[i] === '[' || s[i] === '{') openBracket.push(s[i]);
+    const checkStack = [];
+
+    for(const char of s){
+        if(char === '(' || char === '[' || char === '{') checkStack.push(char);
         else{
-            if(s[i] === ')'){
-                if(openBracket[openBracket.length-1] === '(') openBracket.pop();
-                else return false;
-            }else if(s[i] === ']'){
-                if(openBracket[openBracket.length-1] === '[') openBracket.pop();
-                else return false;
-            }else if(s[i] === '}'){
-                if(openBracket[openBracket.length-1] === '{') openBracket.pop();
-                else return false;
-            }
+            if(bracketPairs[char] !== checkStack.pop()) return false;
         }
     }
-
-    if(openBracket.length === 0) return true;
-    else return false;
+    return checkStack.length === 0; // case: s = ")",  s = "(";
 };
 
 let s = "()[]{}";
