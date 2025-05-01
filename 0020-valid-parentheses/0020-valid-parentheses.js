a152a97517f4be32963287ffce947b9df8f577fb
Eunjung17 <sogoal17@naver.com>
2025-05-01 08:46:31 -0500

    Time: 2 ms (75.95%), Space: 55.1 MB (70.83%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,51 +3,51 @@
  * @param {string} s
  * @return {boolean}
  */
-function isValid(s) {
+// function isValid(s) {
 
-    let openBracket = [];
-    for(let i = 0; i < s.length; i++){
-        if(s[i] === '(' || s[i] === '[' || s[i] === '{') openBracket.push(s[i]);
-        else{
-            if(s[i] === ')'){
-                if(openBracket[openBracket.length-1] === '(') openBracket.pop();
-                else return false;
-            }else if(s[i] === ']'){
-                if(openBracket[openBracket.length-1] === '[') openBracket.pop();
-                else return false;
-            }else if(s[i] === '}'){
-                if(openBracket[openBracket.length-1] === '{') openBracket.pop();
-                else return false;
-            }
-        }
-    }
+//     let openBracket = [];
+//     for(let i = 0; i < s.length; i++){
+//         if(s[i] === '(' || s[i] === '[' || s[i] === '{') openBracket.push(s[i]);
+//         else{
+//             if(s[i] === ')'){
+//                 if(openBracket[openBracket.length-1] === '(') openBracket.pop();
+//                 else return false;
+//             }else if(s[i] === ']'){
+//                 if(openBracket[openBracket.length-1] === '[') openBracket.pop();
+//                 else return false;
+//             }else if(s[i] === '}'){
+//                 if(openBracket[openBracket.length-1] === '{') openBracket.pop();
+//                 else return false;
+//             }
+//         }
+//     }
 
-    if(openBracket.length === 0) return true;
-    else return false;
-};
+//     if(openBracket.length === 0) return true;
+//     else return false;
+// };
 
 /**
  * Second Code
  * @param {string} s
  * @return {boolean}
  */
-// function isValid(s) {
-//     const bracketPairs = {
-//         ')' : '(',
-//         ']' : '[',
-//         '}' : '{'
-//     };
+function isValid(s) {
+    const bracketPairs = {
+        ')' : '(',
+        ']' : '[',
+        '}' : '{'
+    };
 
-//     const checkStack = [];
+    const checkStack = [];
 
-//     for(const char of s){
-//         if(char === '(' || char === '[' || char === '{') checkStack.push(char);
-//         else{
-//             if(bracketPairs[char] !== checkStack.pop()) return false;
-//         }
-//     }
-//     return checkStack.length === 0; // case: s = ")",  s = "(";
-// };
+    for(const char of s){
+        if(char === '(' || char === '[' || char === '{') checkStack.push(char);
+        else{
+            if(bracketPairs[char] !== checkStack.pop()) return false;
+        }
+    }
+    return checkStack.length === 0; // case: s = ")",  s = "(";
+};
 
 let s = "()[]{}";
 isValid(s);
