2a58fd316d121456aabbafcebbc202acf11a82e8
Eunjung17 <sogoal17@naver.com>
2025-05-07 19:40:43 -0500

    Time: 8 ms (1.14%), Space: 57.8 MB (5.99%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.js
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -6,6 +6,12 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+//  function TreeNode(val, left, right) {
+//      this.val = (val===undefined ? 0 : val)
+//      this.left = (left===undefined ? null : left)
+//      this.right = (right===undefined ? null : right)
+// }
+
 /**
  * @param {TreeNode} root
  * @return {TreeNode}
@@ -21,6 +27,11 @@ var invertTree = function(root) {
 
     invertTree(root.left);
     invertTree(root.right);
-    
+    console.log("root:",  root);
     return root;
 };
+
+// let root = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+
+// let result = new invertTree(root);
+// console.log(JSON.stringify(result,null,2));
