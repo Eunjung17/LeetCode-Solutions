60e2e59941789f2471896577cf26b85551e15eb0
Eunjung17 <sogoal17@naver.com>
2025-05-15 14:34:29 -0500

    Time: 41 ms (54.46%), Space: 60.4 MB (52.29%) - LeetHub

--- 0561-array-partition/0561-array-partition.js
+++ 0561-array-partition/0561-array-partition.js
@@ -1,9 +1,12 @@
 /** 
 [key point]
-- Sort the number ascentically
+- Sort the number ascending
 - If we have a sum of maxium for each pair of minimum(num1, num2):
     - make pairs from beginning of nums[0], nums[1] ....nums[nums.length-2], nums[nums.length-1]
     Because, min(1, 1000) is 1 but min(999, 1000) is 999 
+
+Time Complexity: O(NLogN)
+Space Complexity: O(1)
 */
 
 /**
@@ -24,5 +27,29 @@ var arrayPairSum = function(nums) {
         i += 2;
     }
 
+    return maxSum;
+};
+
+/** 
+[improved part]
+
+Eventually, we just need to sum of the values at even indices
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function(nums) {
+
+    if(nums.length < 2) return nums;
+
+    nums.sort((a,b)=> a-b);
+
+    let maxSum = 0;
+
+    for(let i = 0 ; i < nums.length ; i +=2){
+        maxSum += nums[i];
+    }
     return maxSum;
 };
