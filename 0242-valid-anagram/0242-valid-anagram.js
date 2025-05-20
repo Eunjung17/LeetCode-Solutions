8f6f945f38273c4b818df315a74aaa906d78d462
Eunjung17 <sogoal17@naver.com>
2025-05-20 08:15:52 -0500

    Time: 10 ms (82.61%), Space: 59.8 MB (23.37%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -2,11 +2,35 @@
  * @param {string} s
  * @param {string} t
  * @return {boolean}
- */
-var isAnagram = function(s, t) {
-    const stringS = s.split('').sort().join('');
-    const stringT = t.split('').sort().join('');
 
-    if(stringS === stringT) return true;
-    else return false;
+ Time Complexity: O(NLogN)
+ Space Complexity: O(N)
+ */
+// var isAnagram = function(s, t) {
+//     const stringS = s.split('').sort().join('');
+//     const stringT = t.split('').sort().join('');
+
+//     if(stringS === stringT) return true;
+//     else return false;
+// };
+
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+
+    const S = new Map();
+    const T = new Map();
+
+    for (let char of s) {
+        S.set(char, (S.get(char) || 0) + 1);
+    }
+
+    for (let char of t) {
+        T.set(char, (T.get(char) || 0) + 1);
+    }
+
+    for (let [key, value] of S) {
+        if (T.get(key) !== value) return false;
+    }
+
+    return true;
 };
