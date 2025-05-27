d0be0835519f17172a6e35cd9cbe67e5780255cc
Eunjung17 <sogoal17@naver.com>
2025-05-27 11:03:38 -0500

    Time: 48 ms (79.52%), Space: 59.2 MB (54.15%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+    Tortois and Hare Algorithm
+*/
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let slow = head;
+    let fast = head;
+
+    while(fast != null && fast.next != null){
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if(fast === slow) return true;
+    }
+    return false;
+};
