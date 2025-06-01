baf0397d8f87243513906d1b33c4cb306534ae99
Eunjung17 <sogoal17@naver.com>
2025-06-01 11:28:52 -0500

    Time: 367 ms (96.83%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,17 +1,27 @@
 # Write your MySQL query statement below
 
 /*
-Using LAG() function
+    Using LAG() function
 */
 
-WITH preWeather AS(
-    SELECT id, recordDate, temperature, 
-            LAG(recordDate, 1) OVER(ORDER BY recordDate) as prevDate,
-            LAG(temperature, 1) OVER(ORDER BY recordDate) as prevTemperature
-    FROM Weather
-)
+-- WITH preWeather AS(
+--     SELECT id, recordDate, temperature, 
+--             LAG(recordDate, 1) OVER(ORDER BY recordDate) as prevDate,
+--             LAG(temperature, 1) OVER(ORDER BY recordDate) as prevTemperature
+--     FROM Weather
+-- )
 
-SELECT id
-FROM preWeather
-WHERE temperature  > prevTemperature 
-AND recordDate = DATE_ADD(prevDate, INTERVAL 1 DAY);
+-- SELECT id
+-- FROM preWeather
+-- WHERE temperature  > prevTemperature 
+-- AND recordDate = DATE_ADD(prevDate, INTERVAL 1 DAY);
+
+/*
+    Using Subquery
+*/
+
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2
+ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
+WHERE w1.temperature  > w2.temperature 
