de6e70b527d08ed89599fd3efa58104a9aa02f06
Eunjung17 <sogoal17@naver.com>
2025-06-01 11:33:56 -0500

    Time: 431 ms (77.59%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -10,20 +10,15 @@
 --             LAG(temperature, 1) OVER(ORDER BY recordDate) as prevTemperature
 --     FROM Weather
 -- )
-
 -- SELECT id
 -- FROM preWeather
 -- WHERE temperature  > prevTemperature 
 -- AND recordDate = DATE_ADD(prevDate, INTERVAL 1 DAY);
 
 
--- SELECT w1.id
--- FROM Weather w1
--- JOIN Weather w2
--- ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
--- WHERE w1.temperature  > w2.temperature 
 
 SELECT w1.id
-FROM Weather w1, Weather w2
-WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1
-AND w1.temperature > w2.temperature;
+FROM Weather w1
+JOIN Weather w2
+ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
+WHERE w1.temperature  > w2.temperature 
