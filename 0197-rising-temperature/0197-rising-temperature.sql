ac6385133c6c884d7ff6528811fbdb8326e46145
Eunjung17 <sogoal17@naver.com>
2025-06-01 11:34:15 -0500

    Time: 387 ms (90.33%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -4,21 +4,21 @@
     Using LAG() function
 */
 
--- WITH preWeather AS(
---     SELECT id, recordDate, temperature, 
---             LAG(recordDate, 1) OVER(ORDER BY recordDate) as prevDate,
---             LAG(temperature, 1) OVER(ORDER BY recordDate) as prevTemperature
---     FROM Weather
--- )
--- SELECT id
--- FROM preWeather
--- WHERE temperature  > prevTemperature 
--- AND recordDate = DATE_ADD(prevDate, INTERVAL 1 DAY);
+WITH preWeather AS(
+    SELECT id, recordDate, temperature, 
+            LAG(recordDate, 1) OVER(ORDER BY recordDate) as prevDate,
+            LAG(temperature, 1) OVER(ORDER BY recordDate) as prevTemperature
+    FROM Weather
+)
+SELECT id
+FROM preWeather
+WHERE temperature  > prevTemperature 
+AND recordDate = DATE_ADD(prevDate, INTERVAL 1 DAY);
 
 
 
-SELECT w1.id
-FROM Weather w1
-JOIN Weather w2
-ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
-WHERE w1.temperature  > w2.temperature 
+-- SELECT w1.id
+-- FROM Weather w1
+-- JOIN Weather w2
+-- ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
+-- WHERE w1.temperature  > w2.temperature 
