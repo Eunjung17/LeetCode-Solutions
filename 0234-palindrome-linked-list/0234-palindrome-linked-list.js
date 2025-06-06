61595374e541c4e49cc5cd187deb05cdb1cd5df5
Eunjung17 <sogoal17@naver.com>
2025-06-06 11:08:44 -0500

    Time: 3 ms (93.81%), Space: 75.9 MB (80.61%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.js
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -24,31 +24,31 @@ Fail: Space Complexity: O(N) should be O(1)
  * @param {ListNode} head
  * @return {boolean}
  */
-var isPalindrome = function(head) {
-    let valArray = [];
-    let middle = 0;
-    let curCompareIndex = 0;
+// var isPalindrome = function(head) {
+//     let valArray = [];
+//     let middle = 0;
+//     let curCompareIndex = 0;
 
-    // Convert Linked-list into Array-list
-    while(head){
-        valArray.push(head.val);
-        head = head.next;
-    }
+//     // Convert Linked-list into Array-list
+//     while(head){
+//         valArray.push(head.val);
+//         head = head.next;
+//     }
     
-    // end-index
-    curCompareIndex = valArray.length - 1;
+//     // end-index
+//     curCompareIndex = valArray.length - 1;
 
-    // middle-index
-    middle = Math.floor(valArray.length / 2);
+//     // middle-index
+//     middle = Math.floor(valArray.length / 2);
     
-    // Compare each of indice with last of index, moving backword
-    for(let i = 0 ; i < middle ; i++){ 
-        if(valArray[i] !== valArray[curCompareIndex]) return false;
+//     // Compare each of indice with last of index, moving backword
+//     for(let i = 0 ; i < middle ; i++){ 
+//         if(valArray[i] !== valArray[curCompareIndex]) return false;
 
-        curCompareIndex--;
-    }
-    return true;
-};
+//         curCompareIndex--;
+//     }
+//     return true;
+// };
 
 /** 
 1. Convert Linked-list into Array using While-loop
@@ -59,5 +59,48 @@ Time Complexity: O(N)
 Space Complexity: O(N)
 
 Fail: Space Complexity: O(N) should be O(1)
-
+- Instead of using arrays, I should manipulate the linked list directly.
 */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let slow = head;
+    let fast = head;
+    let current = null;
+    let previous = null;
+
+    // Find start of the middle node
+    while(fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+       // 홀수 길이면 중간 노드는 건너뜀
+    if (fast) slow = slow.next;
+
+    current = slow;
+
+    // Make a seperate with left node & reverse them
+    while(current){
+        
+        let nextNode = current.next;
+        current.next = previous; 
+        previous = current;
+        current = nextNode;
+    }
+
+    let leftNode = head;
+    let rightNode = previous;
+
+    // Comparing two node sets
+    while(rightNode){
+        if(leftNode.val !== rightNode.val) return false;
+        leftNode = leftNode.next;
+        rightNode = rightNode.next;
+    }
+
+    return true;
+};
