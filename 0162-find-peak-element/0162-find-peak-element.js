792d0b272dd7dbcb68597ce9e4c490b3045099a8
Eunjung17 <sogoal17@naver.com>
2025-06-07 11:02:47 -0500

    Time: 0 ms (100%), Space: 53.6 MB (53.72%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -1,3 +1,24 @@
+/** 
+- fail to reason
+: You must write an algorithm that runs in O(log n) time.
+: We need to use Binary Search instead of linear iteration
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// var findPeakElement = function(nums) {
+
+//     if(nums.length === 0) return false;
+//     if(nums.length === 1) return 0; 
+
+//     for(let i = 0 ; i < nums.length - 1 ; i++){
+//         if(nums[i] > nums[i+1]) return i;
+//     }
+//     return nums.length - 1;
+// };
+
 /**
  * @param {number[]} nums
  * @return {number}
@@ -7,8 +28,18 @@ var findPeakElement = function(nums) {
     if(nums.length === 0) return false;
     if(nums.length === 1) return 0; 
 
-    for(let i = 0 ; i < nums.length - 1 ; i++){
-        if(nums[i] > nums[i+1]) return i;
+    let left = 0;
+    let right = nums.length - 1;
+
+    while(left !== right){
+        
+        let middle = Math.floor((left + right) /2);
+
+        if(nums[middle] < nums[middle + 1]){
+            left = middle + 1;
+        }else{
+            right = middle;
+        }
     }
-    return nums.length - 1;
+    return right;
 };
