cff3dd08f1c5270f183e11f14fcb2227346063e9
Eunjung17 <sogoal17@naver.com>
2025-06-10 08:11:50 -0500

    Time: 0 ms (100%), Space: 56.7 MB (67.1%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -7,23 +7,38 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var majorityElement = function(nums) {
+
+//     let freqNums = new Map();
+
+//     for(let num of nums){
+//         freqNums.set(num, (freqNums.get(num) || 0) + 1);
+//     }
+
+//     let maxCount = 0;
+//     let majorityElement = null;
+
+//     for(let [key,value] of freqNums){
+//         if(value > maxCount && value > nums.length / 2){
+//             maxCount = value;
+//             majorityElement = key;
+//         }
+//     }
+
+//     return majorityElement;
+// };
+
 var majorityElement = function(nums) {
+    let count = 0;
+    let candidate = null;
 
-    let freqNums = new Map();
-
-    for(let num of nums){
-        freqNums.set(num, (freqNums.get(num) || 0) + 1);
-    }
-
-    let maxCount = 0;
-    let majorityElement = null;
-
-    for(let [key,value] of freqNums){
-        if(value > maxCount && value > nums.length / 2){
-            maxCount = value;
-            majorityElement = key;
+    // 1. 후보 찾기
+    for (let num of nums) {
+        if (count === 0) {
+            candidate = num;
         }
+        count += (num === candidate) ? 1 : -1;
     }
 
-    return majorityElement;
+    return candidate;
 };
