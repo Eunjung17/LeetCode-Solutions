832818607d6b91cf5e2da8e6eabab1490c2339f9
Eunjung17 <sogoal17@naver.com>
2025-06-30 20:38:19 -0500

    Time: 1 ms (79.75%), Space: 59.2 MB (81.27%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,31 @@
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let countZero = 0;
+    let pointer = null;
+
+    for(let i = 0 ; i < nums.length ; i++){
+
+        if(nums[i] === 0 && pointer === null){
+            pointer = i;
+        }
+        
+        if(nums[i] !== 0 && pointer !== null){
+
+            let temp = nums[pointer];
+            nums[pointer] = nums[i];
+            nums[i] = temp;
+            
+            if(nums[pointer + 1] === 0){
+                pointer = pointer + 1;
+            }else{
+                pointer = i;
+            }
+        }
+    }
+
+    return nums;
+};
