e78e684dd410433167e856b93104c5c41ebed8db
Eunjung17 <sogoal17@naver.com>
2025-07-03 10:24:52 -0500

    Time: 470 ms (80.72%), Space: 0B (100%) - LeetHub

--- 0512-game-play-analysis-ii/0512-game-play-analysis-ii.sql
+++ 0512-game-play-analysis-ii/0512-game-play-analysis-ii.sql
@@ -7,20 +7,34 @@
 --     group by a2.player_id
 -- )
 
-WITH
-  min_data AS (
-    SELECT
-      A.player_id,
-      MIN(A.event_date) AS event_date
-    FROM
-      Activity A
-    GROUP BY
+
+
+-- WITH
+--   min_data AS (
+--     SELECT
+--       A.player_id,
+--       MIN(A.event_date) AS event_date
+--     FROM
+--       Activity A
+--     GROUP BY
+--       A.player_id
+--   )
+-- SELECT
+--   A2.player_id,
+--   A2.device_id
+-- FROM
+--   Activity A2
+--   INNER JOIN min_data M ON M.player_id = A2.player_id
+--   AND M.event_date = A2.event_date;
+
+
+SELECT DISTINCT
+  A.player_id,
+  FIRST_VALUE(A.device_id) OVER (
+    PARTITION BY
       A.player_id
-  )
-SELECT
-  A2.player_id,
-  A2.device_id
+    ORDER BY
+      A.event_date
+  ) AS device_id
 FROM
-  Activity A2
-  INNER JOIN min_data M ON M.player_id = A2.player_id
-  AND M.event_date = A2.event_date;
+  Activity A;
