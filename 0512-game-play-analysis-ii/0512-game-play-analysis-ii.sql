6b9b60a7167bb11be496b0ab50dc3de8e6d1f673
Eunjung17 <sogoal17@naver.com>
2025-07-03 10:10:29 -0500

    Time: 493 ms (57.1%), Space: 0B (100%) - LeetHub

--- 0512-game-play-analysis-ii/0512-game-play-analysis-ii.sql
+++ 0512-game-play-analysis-ii/0512-game-play-analysis-ii.sql
@@ -1,9 +1,26 @@
 # Write your MySQL query statement below
-select a1.player_id, a1.device_id
-from Activity a1
-where (a1.player_id, a1.event_date) in (
-    select a2.player_id, MIN(a2.event_date)
-    from Activity a2
-    group by a2.player_id
-)
+-- select a1.player_id, a1.device_id
+-- from Activity a1
+-- where (a1.player_id, a1.event_date) in (
+--     select a2.player_id, MIN(a2.event_date)
+--     from Activity a2
+--     group by a2.player_id
+-- )
 
+WITH
+  min_data AS (
+    SELECT
+      A.player_id,
+      MIN(A.event_date) AS event_date
+    FROM
+      Activity A
+    GROUP BY
+      A.player_id
+  )
+SELECT
+  A2.player_id,
+  A2.device_id
+FROM
+  Activity A2
+  INNER JOIN min_data M ON M.player_id = A2.player_id
+  AND M.event_date = A2.event_date;
